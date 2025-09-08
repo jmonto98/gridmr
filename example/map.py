c78efa931dea044fe0043c0ec472f151195eb51a
jmonto98 <jmonto98@eafit.edu.co>
2025-09-07 20:28:59 -0500

    New project

--- example/map.py
+++
@@ -1,8 +0,0 @@
-import sys
-data = sys.stdin.buffer.read()
-try:
-    text = data.decode("utf-8")
-    out = text.upper()
-    sys.stdout.buffer.write(out.encode("utf-8"))
-except Exception:
-    sys.stdout.buffer.write(data)
--- example/reduce.py
+++
@@ -1,9 +0,0 @@
-import sys
-data = sys.stdin.buffer.read()
-try:
-    text = data.decode("utf-8")
-    lines = text.splitlines()
-    sys.stdout.write("\n".join(lines))
-    sys.stdout.write(f"\n\n-- Total lines: {len(lines)}\n")
-except Exception:
-    sys.stdout.buffer.write(data)
